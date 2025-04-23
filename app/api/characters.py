81fdfcc44efc21bd77049094413d9134b06814e1
musavel <musavelhb@gmail.com>
2025-04-23 09:09:49 +0900

    캐릭터 생성시, 유저 존재 여부 확인

--- app/api/characters.py
+++ app/api/characters.py
@@ -6,6 +6,7 @@ from app.services import characters
 from app.models.characters import CharacterCreateRequest, CharacterCreateResponse
 
 from app.db.characters import Character
+from app.db.users import Users
 from app.db.database import SessionLocal
 
 router = APIRouter(prefix="/characters", tags=["characters"])
@@ -20,6 +21,9 @@ def get_db():
 
 @router.post("/create", response_model=CharacterCreateResponse, status_code=200)
 def create_character(data: CharacterCreateRequest, db: Session = Depends(get_db)):
+    user = db.query(Users).filter(Users.user_id == data.user_id).first()
+    if not user:
+        raise HTTPException(status_code=400, detail="등록되지 않은 사용자입니다.")
     try:
         characters.create_character(data, db)
         return {
