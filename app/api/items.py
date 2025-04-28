37f47ef3e43b56ea37fefe14523d762f163a7be2
musavel <musavelhb@gmail.com>
2025-04-28 14:57:18 +0900

    인벤토리 기능 추가

--- app/api/items.py
+++ app/api/items.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.services import items
 
-from app.models.items import EquipmentUpsertRequest, EquipmentUpsertResponse
+from app.models.items import EquipmentUpsertRequest, EquipmentUpsertResponse, InventoryUpsertRequest, InventoryUpsertResponse
 
 from app.db.database import SessionLocal
 
@@ -26,6 +26,14 @@ def upsert_character_equipment(data: EquipmentUpsertRequest, db: Session = Depen
             }
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
-    
 
+@router.post("/upsert/inventory", response_model=InventoryUpsertResponse, status_code=200)
+def upsert_character_inventory(data: InventoryUpsertRequest, db: Session = Depends(get_db)):
+    try:
+        result = items.upsert_character_inventory(data, db)
+        if result.upserted_id or result.modified_count:
+            return {
+                "message": "인벤토리 반영 완료"
+            }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- app/api/me.py
+++ app/api/me.py
@@ -5,10 +5,10 @@ from uuid import UUID
 from app.db.database import SessionLocal
 
 from app.models.characters import CharacterInfoRequest, CharacterInfoResponse
-from app.models.items import EquipmentGetRequest
+from app.models.items import EquipmentGetRequest, InventoryGetRequest
 
 from app.services.characters import get_character
-from app.services.items import get_character_equipment
+from app.services.items import get_character_equipment, get_character_inventory
 
 router = APIRouter(prefix="/me", tags=["me"])
 
@@ -54,10 +54,12 @@ def get_me(request: CharacterInfoRequest, db: Session = Depends(get_db)):
         }
 
         equipment_info = get_character_equipment(EquipmentGetRequest(character_id=character.character_id), db)
-        
+        inventory_info = get_character_inventory(InventoryGetRequest(character_id=character.character_id), db)
+
     return {
         "message": "정보 조회 완료"
         , "user_id": str(request.user_id)
         , "character_info": character_info
         , "equipment_info": equipment_info
+        , "inventory_info": inventory_info
     }
